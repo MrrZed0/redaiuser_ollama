e5cd4eba530968dff0eafc6336ccc2f13b0d3742
MrrZed0 <154046655+MrrZed0@users.noreply.github.com>
2025-07-13 18:59:41 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start my test bot
 source ./.venv/bin/activate
-redbot dev_bzbot --dev --debug # --rpc
+redbot redaiuser_ollama --dev --debug # --rpc
 
 # source ./.dashboard-venv/bin/activate
 # reddash
